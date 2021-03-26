2b19a886d6869bef8c592037c105032fb672ef65
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-26 20:59:19 +0100

    [feature] Develop setup-wsl-ssh-agent.sh script

--- windows/setup-wsl-ssh-agent.sh
+++ windows/setup-wsl-ssh-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e -x
+set -e
 
 warn () {
     if [ -n "$1" ]; then
@@ -33,11 +33,110 @@ wsl_version() {
     esac
 }
 
+find_npiperelay() {
+    npiperelay="$(which npiperelay.exe)"
+    case "$npiperelay" in
+        "") fatal << 'CANNOT_FIND_NPIPERELAY' ;;
+Fatal: npiperelay.exe not found.
+
+Please make sure that this script resides in the same directory as the
+npiperelay.exe utility from the wsl-ssh-agent release.
+
+CANNOT_FIND_NPIPERELAY
+    esac
+}
+
+ensure_socat() {
+    if which socat >/dev/null 2>&1; then return 0; fi
+
+    # You did install Ubuntu as the documentation told you, right?
+    (
+        set -x
+        sudo apt -qy update
+        sudo apt -qy install socat
+    )
+}
+
+ensure_ssh_add() {
+    if which ssh-add >/dev/null 2>&1; then return 0; fi
+
+    fatal <<'NO_SSH_ADD'
+Fatal: the `ssh-add` command is missing. Cannot continue.
+NO_SSH_ADD
+}
+
+ensure_ssh_dir() {
+    local ssh_dir="$HOME"/.ssh
+    if [ ! -d "$ssh_dir" ]; then mkdir "$ssh_dir"; fi
+    chmod 0700 "$ssh_dir"
+}
+
+ssh_add_already_works() {
+    if ssh_add >/dev/null 2>&1; then
+        return 0
+    elif [ "$?" = 1 ]; then
+        # "The agent has no identities."
+        return 0
+    else
+        return $?
+    fi
+}
+
+setup_dotprofile() {
+    dotprofile="$HOME"/.profile
+    if grep SSH_AUTH_SOCK "$dotprofile"; then
+        fatal <<U_CANTTOUCHTHIS
+Fatal: SSH_AUTH_SOCK configuration already present in $dotprofile
+Bailing out
+U_CANTTOUCHTHIS
+    fi
+
+    cat >> "$dotprofile" <<DOTPROFILE_SNIPPET
+
+export SSH_AUTH_SOCK=\$HOME/.ssh/agent.sock
+
+ss -a | grep -q $SSH_AUTH_SOCK
+if [ \$? -ne 0  ]; then
+    rm -f $SSH_AUTH_SOCK
+    ( setsid socat "UNIX-LISTEN:\$SSH_AUTH_SOCK,fork" "EXEC:\"$npiperelay\" -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+fi
+
+DOTPROFILE_SNIPPET
+
+}
+
 main() {
     case "$(wsl_version)" in
         "") fatal "This does not appear to be a WSL Linux. Bailing out" ;;
         1)  fatal "Only WSL v2 is supported (on account of Keybase-specific requirements)" ;;
     esac
+
+    ensure_ssh_add
+    if ssh_add_already_works; then
+        warn <<NOTHING_TO_DO
+Your ssh agent appears to be functional already; nothing to do:
+
+`ssh-add -l`
+
+NOTHING_TO_DO
+        exit 0
+    fi
+
+    export PATH="$(dirname "$0"):$PATH"
+    find_npiperelay
+
+    ensure_socat
+    ensure_ssh_dir
+    setup_dotprofile
+
+    cat >&2 <<ALL_DONE
+Configuration all done in $dotprofile
+
+Please log out of Windows® and then back in, and try
+
+  ssh-add -l
+
+ALL_DONE
 }
 
 
