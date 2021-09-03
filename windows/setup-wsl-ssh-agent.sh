137bcaf31a4f6bc8334d724580763fac509e71e4
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-09-03 09:05:49 +0200

    [fix] Defective quoting

--- windows/setup-wsl-ssh-agent.sh
+++ windows/setup-wsl-ssh-agent.sh
@@ -129,9 +129,9 @@ U_CANTTOUCHTHIS
 
 export SSH_AUTH_SOCK=\$HOME/.ssh/agent.sock
 
-ss -a | grep -q $SSH_AUTH_SOCK
+ss -a | grep -q \$SSH_AUTH_SOCK
 if [ \$? -ne 0  ]; then
-    rm -f $SSH_AUTH_SOCK
+    rm -f \$SSH_AUTH_SOCK
     ( setsid socat "UNIX-LISTEN:\$SSH_AUTH_SOCK,fork" "EXEC:\"$npiperelay\" -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
 fi
 
