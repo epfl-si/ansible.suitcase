c90f4a13fe62310c5c09188bd0d13a9c750788d3
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-03-27 17:56:57 +0100

    [fix] Throw in some `realpath`

--- windows/setup-wsl-ssh-agent.sh
+++ windows/setup-wsl-ssh-agent.sh
@@ -132,7 +132,7 @@ export SSH_AUTH_SOCK=\$HOME/.ssh/agent.sock
 ss -a | grep -q \$SSH_AUTH_SOCK
 if [ \$? -ne 0  ]; then
     rm -f \$SSH_AUTH_SOCK
-    ( setsid socat "UNIX-LISTEN:\$SSH_AUTH_SOCK,fork" "EXEC:\"$npiperelay\" -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+    ( setsid socat "UNIX-LISTEN:\$SSH_AUTH_SOCK,fork" "EXEC:\"$(realpath "$npiperelay")\" -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
 fi
 
 DOTPROFILE_SNIPPET
